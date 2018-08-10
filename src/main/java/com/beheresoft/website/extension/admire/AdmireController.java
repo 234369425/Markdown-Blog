c9348ed0bafe88c38153b0f57d29547f4af58bfc
Aladi <234369425@qq.com>
2018-08-10 18:27:36 +0800

    remove

--- src/main/java/com/beheresoft/website/extension/admire/AdmireController.java
+++ src/main/java/com/beheresoft/website/extension/admire/AdmireController.java
@@ -1,6 +1,5 @@
 package com.beheresoft.website.extension.admire;
 
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
